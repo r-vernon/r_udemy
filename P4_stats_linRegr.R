80fd9b8b965f643988ce7968a90289637d2e5bf3
Richard Vernon <40852586+r-vernon@users.noreply.github.com>
2023-02-28 15:23:46 +0000

    added notes on linear regression

---
+++ P4_stats_linRegr.R
@@ -0,0 +1,58 @@
+# NOTES FOR LINEAR REGRESSION
+# ---------------------------
+#  
+# Estimating Y ~ b0 + b1X1 + ... + bnXn + err.
+# - typically do so by min. Residual Sum of Squares (RSS)
+#
+# note on multiple regression:
+# - when we have multiple estimates (b1, b2, ..., bn) the size of (e.g.) b1
+#   reflects the effect of x1 holding other variables (x2...xn) fixed
+#  - this gets complicated when regressors are correlated!
+#
+# say we're taking repeated estimates of a mean from some population
+# - variance of those means = variance(population)/n
+#  - as n increases (more samples), a given mean will be closer to pop. mean
+# - can sqrt(var) to get SE (Std. Err.): SE = SD/sqrt(n)
+#  - (commonly used for confidence intervals: xbar +/- 1.96SEM)
+#
+# can calculate SE (squared) for b0, b1 with:
+# - SE(B0)^2 = (SD^2) * [(1/n) + ((xbar^2) / sum((xi - xbar)^2))]
+# - SE(B1)^2 = (SD^2) / sum((xi-xbar)^2)
+#  - note that SD should be pop. SD, but usually est. with sample SD
+#  - call this RSE (Residual Standard Error): RSE = sqrt(RSS/(n-p-1))
+#  - (dof = n-2 for basic model as p = 1 (for b1))
+# - can then use these SE estimates to estimate significance
+#  - e.g. t-statistic t(b1) = (b1-0) / SE(b1)... with dof = n-2
+#
+# accuracy of the model...
+# - already know RSS: Residual Sum Squares: sum((yi - f(xi))^2)
+#  - that describes unexplained variance in the model
+# - we know know RSE: sqrt(RSS/(n-2))
+#  - that estimates error even if the model parameters were estimated perfectly
+#    (the '+ err' term that reflects noise that cannot be captured)
+#  - if RSE is small relative to (say) mean of data, decent fit!
+# - can also calc. R^2: 1 - (RSS/TSS) (TSS - Total Sum Squares; all var^2)
+#  - R^2 is prop. var. in Y that can be explained with model on X
+# - can also calc. F-statistic as estimate for whole model
+#  - F = ((TSS-RSS)/p) / (RSS/(n-p-1)) (p: nvars, n: nobs)
+#  - null hypothesis: b1=b2=...=bn=0, F > 1 suggests good pred.
+#
+# for categorical predictors can dummy code
+# - need (numLevels-1) dummy variables to code a category
+# can also code interactions
+# - Y = b0 + b1X1 + b2X2 + b3X1X2 + err. would model X1-X2 interaction
+#  - if an interaction is sig., include components even if not sig.
+# can add polynomial regressors (b0X1 + b1X1^2) to capture limited non-linearity
+#
+# assumptions:
+# - data is linear! (plot resid vs x or pred(x), look for patterns)
+# - errors should be uncorrelated (error ei should not predict error ei+1)
+#  - plot e.g. errors over time to look for trends (if t-series data)
+# - errors should have constant variance (heteroscedasticity)
+#  - funnel shape in residual plot, may be able to transform Y to fix
+# - check for outliers (plot studentized resids. - resids/SE, check > 3)
+# - check for unduly influential data points using a 'leverage statistic'
+# - collinearity (ugh)
+#  - check corr. matrix (but may not detect multicollinearity)
+#  - can calculate VIF (variance inflation factor), >5-10 = issues!
+#   - fix by dropping preds or combinging them (via z-scores, PCA etc)
